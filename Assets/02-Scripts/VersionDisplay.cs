720655374dbde170842837e7fb9cca981ab37c7b
i000Tea <107682438+i000Tea@users.noreply.github.com>
2024-12-07 15:21:27 +0800

    配置优化问题

--- Assets/02-Scripts/VersionDisplay.cs
+++ Assets/02-Scripts/VersionDisplay.cs
@@ -6,12 +6,20 @@ using UnityEngine;
 public class VersionDisplay : MonoBehaviour
 {
    public string msg = "开发中版本:";
+   private void Awake()
+   {
+      Application.targetFrameRate = 60;
+   }
    void Start()
    {
       if (transform.TryGetComponent(out TMP_Text text))
       {
          text.text = msg + Application.version;
       }
+   }
+   private void Update()
+   {
+      Debug.Log(Application.targetFrameRate);
    }
 
 }
